a1e345dc35122a0f3c4cefb6729ba3b9f93a2aa7
Manogya <manogya57@gmail.com>
2023-09-26 15:33:08 +0545

    adder CORS headers. need to add algorithm here

--- backend/server.py
+++ backend/server.py
@@ -2,26 +2,47 @@ import http.server
 import socketserver
 import json
 
-# Specify the port on which your server should listen
+# Define the port where the server will listen
 PORT = 8080
 
+# Create a custom request handler to handle incoming requests
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_OPTIONS(self):
+        # Set the CORS headers to allow requests from your frontend URL
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:5500')  # Replace with your frontend URL
+        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')  # Include OPTIONS
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        data = json.loads(post_data.decode('utf-8'))
 
-        # Print the received data
-        print("Received data:")
-        print(data)
+        # Parse the JSON data received from the client
+        try:
+            data = json.loads(post_data.decode('utf-8'))
+            print("Received data:")
+            print(data)
+            
+            # Modify the response_data to include both the message and the received data
+            response_data = {
+                "message": "Data received by the server",
+                "received_data": data  # Include the received data
+            }
 
-        # You can process the data and send a response here
-
-        # Respond with a simple message
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(bytes("Data received by the server", 'utf-8'))
+            # Set the HTTP status code and send the response data as JSON
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        except json.JSONDecodeError as e:
+            # Handle JSON parsing errors
+            self.send_response(400)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_response = {"error": "Invalid JSON data"}
+            self.wfile.write(json.dumps(error_response).encode('utf-8'))
 
 # Start the server
 with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
