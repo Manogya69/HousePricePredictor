15dd0c2b55305e993382da49f006a5c3ef05a22c
Manogya <manogya57@gmail.com>
2023-09-26 22:01:04 +0545

    wrote code to receive data from the form and display in the console. need to add aglorithm

--- backend/server.py
+++ backend/server.py
@@ -1,36 +1,36 @@
 import http.server
 import socketserver
 import json
-from database_connection import fetch_houses_data  # Import the fetch_houses_data function from your database_connection module
 
 # Define the port where the server will listen
 PORT = 8080
 
-# Create a custom request handler to handle incoming requests
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_OPTIONS(self):
-        # Set the CORS headers to allow requests from your frontend URL
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:5500')  # Replace with your frontend URL
-        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')  # Include OPTIONS
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-        self.end_headers()
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data.decode('utf-8'))
 
-    def do_GET(self):  # Change from POST to GET for fetching data
-        if self.path == '/get_houses':
-            # Set the CORS headers for GET requests
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', 'http://localhost:5500')  # Replace with your frontend URL
-            self.end_headers()
+        # Print the received data
+        print("Received data:")
+        print(data)
 
-            # Fetch data from the database using the fetch_houses_data function
-            houses_data = fetch_houses_data()  # Use the imported function to fetch data from the database
+        
 
-            # Convert data to JSON and send it as a response
-            self.wfile.write(json.dumps(houses_data).encode())
-        else:
-            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        # Process the data here (e.g., prepare it for your algorithm)
+        # You can also send a response back to the client based on the processed data
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Start the server
 with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
