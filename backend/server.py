d6b62315333eed639631b492a7a5c15fa48ffc17
Manogya <manogya57@gmail.com>
2023-09-27 15:56:05 +0545

    unique house data accessed but there are image urls as well, not big problem tho!

--- backend/server.py
+++ backend/server.py
@@ -1,38 +1,34 @@
 import http.server
-import socketserver
 import json
+from database_connection import unique_house_list
 
-# Define the port where the server will listen
-PORT = 8080
+# Global variable to store user preferences
+user_preferences = None
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
+        global user_preferences  # Use the global variable
+
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        data = json.loads(post_data.decode('utf-8'))
+        user_preferences = json.loads(post_data.decode('utf-8'))
 
         # Print the received data
-        print("Received data:")
-        print(data)
+        # print("Received data:")
+        # print(user_preferences)
 
-        
+    def do_GET(self):
+        global user_preferences
 
-        # Process the data here (e.g., prepare it for your algorithm)
-        # You can also send a response back to the client based on the processed data
+        # Print the unique houses when a GET request is made
+        print("Unique Houses:")
+        for house in unique_house_list:
+            print(house)
 
-
-
-
-
-
-
-
-
-
-
-
-
-# Start the server
-with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
+if __name__ == '__main__':
+    try:
+        server = http.server.HTTPServer(('localhost', 8080), MyRequestHandler)
+        print('Server started on http://localhost:8080')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Server stopped.')
